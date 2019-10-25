5fc74e6a49cce66d140e910cab76f88fb009e54d
Jarrod Rizor <me@jarrodrizor.com>
2019-10-25 00:36:03 -0400

    Added basic structure to command line utility to build off of. Added hard coded interface and new_mac_address variables and print statements to practice real world uses.

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,10 @@
 
 import subprocess
 
-subprocess.call("ip addr", shell=True)
+interface = "enp0s3"
+new_mac_address = "00:01:02:03:04:05"
+print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
+
+# subprocess.call("ip link set interface down", shell=True)
+# subprocess.call("ip link set interface address 00:01:02:03:04:05", shell=True)
+# subprocess.call("ip link set interface up", shell=True)
