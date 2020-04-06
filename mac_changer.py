ca0a78a2ff35aa48bf0727152d1bc0f1c3130d7a
JarrodRizor <me@jarrodrizor.com>
2020-04-06 15:38:01 -0400

    added optparse to the program to allow the user to input values as arguments and provide helpful information to the user.

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,17 @@
 #!/usr/bin/#!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("interface > ")
-new_mac_address = input("new MAC > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interfact to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac_address", help="New MAC Address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac_address = options.new_mac_address
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
 
