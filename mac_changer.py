ad272176a24a29c7ace7db9b289bd9034f1a80a2
Jarrod Rizor <me@jarrodrizor.com>
2020-04-06 22:12:50 -0400

    refactored code into functions to make program cleaner.

--- mac_changer.py
+++ mac_changer.py
@@ -3,24 +3,22 @@
 import subprocess
 import argparse
 
-#Create Parser object to store Arguments in
-parser = argparse.ArgumentParser("Change Mac Address")
 
-# Parser Arguments values
-parser.add_argument("-i", "--interface", metavar="", required=True, dest="interface",
-                    help="Select Interface you wish to change MAC Address on.")
-parser.add_argument("-m", "--mac", metavar="", required=True, dest="new_mac_address",
-                    help="What you would like the MAC Address to be.")
+def get_arguments():
+    parser = argparse.ArgumentParser("Change Mac Address")
+    parser.add_argument("-i", "--interface", metavar="", required=True, dest="interface",
+                        help="Select Interface you wish to change MAC Address on.")
+    parser.add_argument("-m", "--mac", metavar="", required=True, dest="new_mac_address",
+                        help="What you would like the MAC Address to be.")
+    return parser.parse_args()
 
-args = parser.parse_args()
 
-#Storing data given from CLI Arguments
-interface = args.interface
-new_mac_address = args.new_mac_address
+def change_mac(interface, new_mac_address):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
+    subprocess.call(["ip", "link", "set", "dev", interface, "down"])
+    subprocess.call(["ip", "link", "set", "dev", interface, "address", new_mac_address])
+    subprocess.call(["ip", "link", "set", "dev", interface, "up"])
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
 
-# Executing IP Command to take down interface, change MAC Address and put it back up.
-subprocess.call(["ip", "link", "set", "dev", interface, "down"])
-subprocess.call(["ip", "link", "set", "dev", interface, "address", new_mac_address])
-subprocess.call(["ip", "link", "set", "dev", interface, "up"])
+args = get_arguments()
+change_mac(args.interface, args.new_mac_address)
