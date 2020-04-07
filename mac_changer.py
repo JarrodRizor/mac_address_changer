e39cec69ea7f28531c9d53596483abedb230da0c
Jarrod Rizor <me@jarrodrizor.com>
2020-04-06 20:18:15 -0400

    removed optparse since I found out it is depricated. Replaced with argparse instead.

--- mac_changer.py
+++ mac_changer.py
@@ -1,21 +1,25 @@
 #!/usr/bin/#!/usr/bin/env python
 
 import subprocess
-import optparse
 import argparse
 
-parser = optparse.OptionParser()
+#Create Parser object to store Arguments in
+parser = argparse.ArgumentParser("Change Mac Address")
 
-parser.add_option("-i", "--interface", dest="interface", help="Interfact to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac_address", help="New MAC Address")
+# Parser Arguments values
+parser.add_argument("-i", action="store", dest="interface",
+                    help="Select Interface you wish to change MAC Address on.")
+parser.add_argument("-m", action="store", dest="new_mac_address",
+                    help="What you would like the MAC Address to be.")
+args = parser.parse_args()
 
-(options, arguments) = parser.parse_args()
-
-interface = options.interface
-new_mac_address = options.new_mac_address
+#Storing data given from CLI Arguments
+interface = args.interface
+new_mac_address = args.new_mac_address
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
 
+# Executing IP Command to take down interface, change MAC Address and put it back up.
 subprocess.call(["ip", "link", "set", "dev", interface, "down"])
 subprocess.call(["ip", "link", "set", "dev", interface, "address", new_mac_address])
 subprocess.call(["ip", "link", "set", "dev", interface, "up"])
