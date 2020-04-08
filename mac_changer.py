065df2a05b22067e36705fb125700f5ce5e675cd
Jarrod Rizor <me@jarrodrizor.com>
2020-04-07 20:05:08 -0400

    added function to check what the current mac address is and to display more information to the end user about their MAC address changing. Uses regex to filter the address and the decode utf-8 is so python3 can peform the regex search.

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import argparse
+import re
 
 
 def get_arguments():
@@ -20,5 +21,23 @@ def change_mac(interface, new_mac_address):
     subprocess.call(["ip", "link", "set", "dev", interface, "up"])
 
 
-args = get_arguments()
-change_mac(args.interface, args.new_mac_address)
+def get_current_mac(interface):
+    iplink = subprocess.check_output(["ip", "link", "show", interface]).decode("utf-8")
+    mac_address_search_result = re.search(r"([0-9a-fA-F]:?){12}", iplink)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+
+print("Current MAC = " + str(get_current_mac(get_arguments().interface)))
+
+change_mac(get_arguments().interface, get_arguments().new_mac_address)
+
+current_mac = get_current_mac(get_arguments().interface)
+
+if current_mac == get_arguments().new_mac_address:
+    print("[+} MAC Address was successfully changed to " + get_arguments().new_mac_address)
+else:
+    print("[-] MAC address did not change.")
